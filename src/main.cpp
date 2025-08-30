42a39716bf050be696d0ea0b24f91cba7086fdcb
Crystic <vilius817@gmail.com>
2025-08-30 16:26:38 +0300

    new file:   .gitignore 	modified:   Makefile 	new file:   graphics/bg.bmp 	new file:   graphics/bg.json 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include "bn_core.h"
-
+#include "bn_regular_bg_ptr.h"
+#include "bn_regular_bg_items_bg.h"
 int main()
 {
     bn::core::init();
-
+    bn::regular_bg_ptr regular_bg = bn::regular_bg_items::bg.create_bg(0, 0);
     while(true)
     {
         bn::core::update();
